d981b326aa31b85ec08d2ae535d99288f933eb45
Ilasbintang <ilasbintang727@gmail.com>
2026-04-20 18:29:12 +0700

    commit 7

--- tugas.cpp
+++ tugas.cpp
@@ -9,6 +9,13 @@ public:
     string kategori;
     string tanggalProduksi;
 
-   
+    // Method untuk menampilkan data barang
+    void tampilkanData() {
+        cout << "Nama Barang       : " << nama << endl;
+        cout << "Jumlah            : " << jumlah << endl;
+        cout << "Kategori          : " << kategori << endl;
+        cout << "Tanggal Produksi  : " << tanggalProduksi << endl;
+        cout << "-----------------------------" << endl;
+    }
 };
 
