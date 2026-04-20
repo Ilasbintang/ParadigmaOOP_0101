53f6fa87788e82f23e857f3d0c086e876ae6bf25
Ilasbintang <ilasbintang727@gmail.com>
2026-04-20 18:29:01 +0700

    commit 6

--- tugas.cpp
+++ tugas.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Membuat class Barang
+class Barang {
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalProduksi;
 
+   
+};
 
