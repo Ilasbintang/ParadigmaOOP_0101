d3c333f928aa3bd671ba8a2ab315477642de81e9
Ilasbintang <ilasbintang727@gmail.com>
2026-04-20 17:31:09 +0700

    commit 1

--- introductionOOp.cpp
+++ introductionOOp.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 using namespace std;
+
 class Mahasiswa{
     public:
     int nim;
     string nama;
     float nilai;
 
-    void prindata(){
-        cout << "Nim :" << nim << endl;
-        cout << "Nama :" << nama << endl;
-        cout << "Nilai :" << nilai << endl;
-
-    }
-    
+  
 };
-int main(){
-    Mahasiswa mhs;
-     mhs.nim = 2024;
-     mhs.nama = "abrs";
-     mhs.nilai = 80.5;
-
-     mhs.prindata();
-
-}
